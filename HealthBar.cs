152b5cbae6d617a65d3737abe8081cac7150b73c
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-01-25 19:32:18 +0300

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -4,21 +4,21 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Health))]
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private Slider _healthBar;
-    [SerializeField] private Health _health;
+    [SerializeField] private Slider _wellnessBar;
+    [SerializeField] private Health _wellness;
 
     private void OnEnable()
     {
-        _health.Changed += OnHealthChanged;
+        _wellness.Changed += OnWellnessChanged;
     }
 
     private void OnDisable()
     {
-        _health.Changed -= OnHealthChanged;
+        _wellness.Changed -= OnWellnessChanged;
     }
 
-    private void OnHealthChanged(float health)
+    private void OnWellnessChanged(float health)
     {
-        _healthBar.value = health;
+        _wellnessBar.value = health;
     }
 }
