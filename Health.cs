8f9a62d4ac5466417cb78f4de5127f2c11957533
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-01-26 15:50:06 +0300

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -6,27 +6,14 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _wellnessIndicator;
     [SerializeField] private float _wellness;
-    [SerializeField] private int _wellnessTextBar;
-
-    private int _amountOfWellness = 100;
-    private int _maximumWellness = 100;
-    private int _minimumWellness = 0;
 
     public event UnityAction<float> Changed;
-    public event UnityAction<int> Modified; 
 
     public void Heal()
     {
         float targetValue = ChangeWellness(_wellness);
 
         Changed?.Invoke(targetValue);
-
-        if (_amountOfWellness < _maximumWellness)
-        {
-            int targetIndicator = ModifieWellness(_wellnessTextBar);
-
-            Modified?.Invoke(targetIndicator);
-        }
     }
 
     public void Damage()
@@ -34,25 +21,11 @@ public class Health : MonoBehaviour
         float targetValue = ChangeWellness(-_wellness);
 
         Changed?.Invoke(targetValue);
-
-        if (_amountOfWellness > _minimumWellness)
-        {
-            int targetIndicator = ModifieWellness(-_wellnessTextBar);
-
-            Modified?.Invoke(targetIndicator);
-        }
     }
 
     private float ChangeWellness(float health)
     {
         float targetValue = _wellnessIndicator.value + health;
-
-        return targetValue;
-    }
-
-    private int ModifieWellness(int health) 
-    { 
-        int targetValue = _amountOfWellness += health;
 
         return targetValue;
     }
